b20ae833d07665e32e4d6881e2ffb5f257f83048
ajaytechryver <ajaysanjy9598@gmail.com>
2023-09-03 18:39:10 +0530

    ajay

---
+++ src/test/java/com/paintcompany/new_demo_proj/AppTest.java
@@ -0,0 +1,13 @@
+package com.paintcompany.new_demo_proj;
+
+/**
+* Hi ajay welcome !
+*
+*/
+public class AppTest
+{
+    public static void main( String[] args )
+    {
+       System.out.println( "Hello world" );
+     }
+}
